85b8546a7d09072de665fd5991b01e10a62cf1ff
datawiz168 <140295050+datawiz168@users.noreply.github.com>
2023-08-24 17:08:00 +0800

    Update gowarpdocs0.go

--- gowarpdocs0.go
+++ gowarpdocs0.go
@@ -1,73 +1,41 @@
-package main // 定义包名为 main，表示可执行程序的入口包
+package main
 
 import (
-	"log" // 导入日志包，用于记录错误或信息
-	"github.com/wiredtiger/go-wiredtiger/wt" // 导入 WiredTiger 的 Golang 绑定
+	"fmt"
+	"sync"
 )
 
-// DB 代表我们的数据库实例
+// DB 表示一个简单的键值数据库
 type DB struct {
-	conn *wt.Conn // WiredTiger 连接对象
+	data map[string]string
+	mu   sync.RWMutex
 }
 
 // NewDB 创建一个新的数据库实例
-func NewDB(path string) (*DB, error) {
-	conn, err := wt.Open(path, "create") // 打开或创建 WiredTiger 连接
-	if err != nil {
-		return nil, err // 返回连接错误
+func NewDB() *DB {
+	return &DB{
+		data: make(map[string]string),
 	}
-
-	return &DB{conn: conn}, nil // 返回数据库实例
 }
 
-// CreateTable 创建一个新的数据表
-func (db *DB) CreateTable(name string) error {
-	session, err := db.conn.OpenSession("") // 打开 WiredTiger 会话
-	if err != nil {
-		return err // 返回会话错误
-	}
-	defer session.Close() // 确保会话在函数结束时关闭
-
-	// 创建数据表
-	_, err = session.Create("table:"+name, "key_format=S,value_format=S")
-	return err // 返回创建表的错误（如果有）
+// Put 存储一个键值对
+func (db *DB) Put(key, value string) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	db.data[key] = value
 }
 
-// Insert 插入一条记录到指定的数据表
-func (db *DB) Insert(table, key, value string) error {
-	session, err := db.conn.OpenSession("") // 打开 WiredTiger 会话
-	if err != nil {
-		return err // 返回会话错误
-	}
-	defer session.Close() // 确保会话在函数结束时关闭
-
-	cursor, err := session.OpenCursor("table:"+table, nil, "") // 打开数据表的游标
-	if err != nil {
-		return err // 返回游标错误
-	}
-	defer cursor.Close() // 确保游标在函数结束时关闭
-
-	cursor.SetKey(key)   // 设置键
-	cursor.SetValue(value) // 设置值
-	return cursor.Insert() // 插入记录并返回任何错误
+// Get 检索一个键的值
+func (db *DB) Get(key string) (string, bool) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+	value, exists := db.data[key]
+	return value, exists
 }
 
 func main() {
-	// 创建数据库实例
-	db, err := NewDB("data")
-	if err != nil {
-		log.Fatalf("Failed to create DB: %v", err) // 记录并退出错误
-	}
-
-	// 创建表
-	if err := db.CreateTable("gowarpdocs"); err != nil {
-		log.Fatalf("Failed to create table: %v", err) // 记录并退出错误
-	}
-
-	// 插入数据
-	if err := db.Insert("gowarpdocs", "key1", "value1"); err != nil {
-		log.Fatalf("Failed to insert data: %v", err) // 记录并退出错误
-	}
-
-	log.Println("Database initialized successfully!") // 记录成功信息
+	db := NewDB()
+	db.Put("name", "GoWarpDocs")
+	value, _ := db.Get("name")
+	fmt.Println("Value:", value) // 输出: Value: GoWarpDocs
 }
